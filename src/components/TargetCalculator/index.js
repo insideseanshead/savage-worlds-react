739c9b8c73e3886f4474d6bb200479bcf2a120ed
Sean Morgan <insideseanshead@gmail.com>
2021-10-11 13:33:46 -0700

    removed list numbers added class and id to list items

--- src/components/TargetCalculator/index.js
+++ src/components/TargetCalculator/index.js
@@ -1,44 +1,50 @@
-import React, {useState, useEffect } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import './style.css'
 
 const TargetCalculator = () => {
-    // Set Target Number
-    const [target, setTarget] = useState(4)
-    //On Change Event
-    const onChange = (e) => {
-        setTarget(parseInt(e, 10))
+  // Set Target Number
+  const [target, setTarget] = useState(4);
+  //On Change Event
+  const onChange = (e) => {
+    setTarget(parseInt(e, 10));
+  };
+  // Function to set Array
+  const hitCalculator = () => {
+    let data = [];
+    let number = target;
+    for (let i = 0; i < 10; i++) {
+      number = number + 4;
+      data.push(number);
     }
-    // Function to set Array 
-    const hitCalculator = () => {
-        let data = [];
-        let number = target
-        for( let i=0 ; i<3; i++){
-            number = number+4
-            data.push(number)
-        }
-        return data
-    }
-    const hitLevel = hitCalculator()
+    return data;
+  };
+  const hitLevel = hitCalculator();
 
-    console.log(hitLevel)
-    
-    return (
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Target Number</Form.Label>
-          <Form.Control type="number" value={target} onChange={(e)=>onChange(e.target.value)} />
-        </Form.Group>
-        
-        <ul className="targetList">
-          {
-            hitLevel.map(function(level,i){
-              return <li key={i}>{level}</li>
-            })
-          }
-        </ul>
-        
-      </Form>
-    )
-}
+  document.querySelectorAll('.targetNumber').forEach()
 
-export default TargetCalculator
+  console.log(hitLevel);
+
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Target Number</Form.Label>
+        <Form.Control
+          type="number"
+          value={target}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </Form.Group>
+
+      <ul className="targetList">
+        {hitLevel.map((level, i) => (
+          <li key={i} className="targetNumber" id={i}>
+            {i + 1}: {level}
+          </li>
+        ))}
+      </ul>
+    </Form>
+  );
+};
+
+export default TargetCalculator;
