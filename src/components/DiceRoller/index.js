e78657386a0351a1ff88d21ad65f2ddf26154a06
Sean Morgan <insideseanshead@gmail.com>
2021-10-13 22:33:09 -0700

    created dice roller component

---
+++ src/components/DiceRoller/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const DiceRoller = () => {
+    return (
+        <Card style='16rem'>
+            <Card.Body>
+                <Card.Title>Dice</Card.Title>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default DiceRoller
