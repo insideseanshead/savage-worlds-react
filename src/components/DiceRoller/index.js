e7957aaff09d28fd4e1ef9df313d45321b0dc5ad
Sean Morgan <insideseanshead@gmail.com>
2021-10-13 23:35:21 -0700

    creating dice exploding logic

--- src/components/DiceRoller/index.js
+++ src/components/DiceRoller/index.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
 import './style.css'
 
 const DiceRoller = () => {
+    // Set State
+    const [roll, setRoll] = useState(0);
+
+    // Set Dice Variables
+    const d4 = 4;
+    const d6 = 6;
+    const d8 = 8;
+    const d10 = 10;
+    const d12 = 12;
+    
+    // Create dice logic dice explode on max
+    const rollDice = (dice) => {
+        let rolled = Math.ceil(Math.random()*dice) 
+        console.log(rolled)
+        let reRolled = 1;
+        if(rolled === dice || reRolled === dice) {
+            reRolled = Math.ceil(Math.random()*dice)
+            console.log(reRolled)
+            rolled = rolled + reRolled;
+        } else {
+            return rolled
+        }
+    }
+    console.log(rollDice(d4))
+   
+    
     return (
         <Card id='diceRollerCard'>
             <Card.Body>
